074714173dbded969c111e81ee8a40dcd4159f1b
Ranwala <URANWRA@PEROOT.COM>
2018-10-02 12:45:10 +0530

    Dev work was completed.

---
+++ src/main/java/org/paumard/ch/four/MainReductionOne.java
@@ -0,0 +1,34 @@
+package org.paumard.ch.four;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+/**
+ *
+ * @author José Paumard
+ */
+public class MainReductionOne {
+
+	public static int reduce(List<Integer> values, int valueIfEmpty, BinaryOperator<Integer> reduction) {
+
+		int result = valueIfEmpty;
+		for (int value : values) {
+			result = reduction.apply(result, value);
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+
+		List<Integer> ints = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+		List<Integer> ints1 = Arrays.asList(0, 1, 2, 3, 4);
+		List<Integer> ints2 = Arrays.asList(5, 6, 7, 8, 9);
+
+		BinaryOperator<Integer> op = (i1, i2) -> i1 + i2;
+		int reduction = reduce(ints, 0, op);
+
+		System.out.println("Reduction : " + reduction);
+	}
+}
